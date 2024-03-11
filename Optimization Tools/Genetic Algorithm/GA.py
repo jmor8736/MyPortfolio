f65d8166b48213eccfb004ba0279e06b8d29adac
jmor8736 <149835716+jmor8736@users.noreply.github.com>
2024-03-11 10:20:43 -0700

    Add files via upload

---
+++ Optimization Tools/Genetic Algorithm/GA.py
@@ -0,0 +1,52 @@
+
+
+import numpy as np
+import random
+
+class Chromosome:
+
+    # Initialize Chromosome:
+    def __init__(self, numGenes):
+        self.genes = random.choices([0, 1], k = numGenes)
+        self.fitness = 0
+
+    # Mutation Function:
+    def mutate(self):
+        gene_loc = random.randint(0, len(self.genes)-1) # select random gene location
+        self.genes[gene_loc] = 1 - self.genes[gene_loc] # flip value of that location
+    
+    # Breeding Method (syntax: child = Chromosome1 + Chromosome2)
+    def __add__(self, o):
+        crossover_point = random.randint(0, len(self.genes)-1) # random crossover point
+        child_genes = self.genes[:crossover_point] + o.genes[crossover_point:] # mix parent genes to create child genes
+        child = Chromosome(len(child_genes)) # initialize child
+        child.genes = child_genes # insert mixed parent genes into child genes
+        return child
+
+
+class Population:
+
+    def __init__(self, populationSize, numGenes):
+        self.members = [Chromosome(numGenes) for i in range(populationSize)]
+
+    def selection(self, ratio):
+        # Step 1 - Sort members by fitness (ascending order)
+        self.members.sort(key=lambda x: x.fitness) # NOTE - this requires you to determine member fitness in script
+
+        # Step 2 - return some number of members based on the ratio provided
+        num_mems = int(ratio*int(len(self.members))) # calculates the number (x) of candidates we will keep
+        self.members = self.members[num_mems:] # reduces the population to the best x candidates
+
+def FitnessFunction(chrom: Chromosome, knapsack):
+
+    # calculate total weight and total value of candidate solution:
+    tot_value = sum(chrom.genes[i]*knapsack.values[i] for i in range(len(chrom.genes)))
+    tot_weight = sum(chrom.genes[i]*knapsack.weights[i] for i in range(len(chrom.genes)))
+
+    # penalize fitness for candidate solutions that exceed the max weight:
+    if tot_weight > knapsack.max_weight:
+        tot_value = 0
+    
+    # return total value as the fitness level:
+    return(tot_value)
+    
---
+++ Optimization Tools/Genetic Algorithm/GA_Validation.py
@@ -0,0 +1,94 @@
+
+
+import GA
+import random
+import statistics as stat
+import matplotlib.pyplot as plt
+
+# define knapsack problem:
+class Knapsack:
+    def __init__(self,values,weights,max_weight):
+        self.values = values
+        self.weights = weights
+        self.max_weight = max_weight
+values = [23,21,8,1,3,7,18,19,17,15,24,22,6,28,4,2,27,20,5,10] # [$]
+weights = [7,2,6,9,1,5,6,1,3,4,7,9,3,7,3,4,5,1,5,4] # [kg]
+max_weight = 45 # [kg]
+knapsack = Knapsack(values, weights, max_weight)
+
+# Variable Inputs ('Knobs')
+Gens = 100
+populationSize = 1000
+selection_ratio = .4
+mutation_percent = .4
+
+mutation_number = int(populationSize*mutation_percent)
+
+# Step 1 - Initialize Population
+myPop = GA.Population(populationSize, numGenes = len(values))
+
+# record best and mean values of valid solutions for initial population:
+fit = [GA.FitnessFunction(mem,knapsack) for mem in myPop.members]
+values = []
+for i in range(len(fit)):
+    if fit[i] != 0:
+        values.append(fit[i])
+mean = [stat.mean(values)]
+best = [max(values)]
+generations = [1]
+
+for i in range(Gens-1):
+
+    # Step 2 - Selection
+    for membs in myPop.members:
+        membs.fitness = GA.FitnessFunction(membs, knapsack)
+    myPop.selection(selection_ratio)
+
+    # Step 3 - Crossover and Mutate
+    # Crossover:
+    num_parents = len(myPop.members)-1
+    while len(myPop.members) < populationSize:
+        p1 = random.randint(0, num_parents) # select parent 1
+        p2 = random.randint(0, num_parents) # select parent 2
+        if p1 != p2: # checks that we are not using the same parent
+            myPop.members.append(myPop.members[p1] + myPop.members[p2])
+
+    # Safegaurd Best Candidate Solution
+    fit = [c.fitness for c in myPop.members] # calculate fitness for new population
+    max_fitness = max(fit)
+    max_index = fit.index(max_fitness)
+
+    # Mutation:
+    for j in range(mutation_number):
+        mutator = random.randint(0, populationSize-1) # index for member to mutate
+
+        # Check that mutator index doesn't corrospond to best value and re-generate index if necessary:
+        if mutator == max_index:
+            while mutator == max_index:
+                mutator = random.randint(0, populationSize-1)
+        
+        # Mutate selected candidate:
+        myPop.members[mutator].mutate()
+
+    # save mean and best values of valid solutions:
+    fit = [GA.FitnessFunction(mem, knapsack) for mem in myPop.members]
+    values = []
+    for k in range(len(fit)):
+        if fit[k] != 0:
+            values.append(fit[k])
+    mean.append(stat.mean(values))
+    best.append(max(values))
+    generations.append(i+2)
+
+    # Step 4 - repeat
+
+
+# Plot Results:
+fig = plt.figure()
+ax = fig.add_subplot(1,1,1)
+ax.plot(generations, mean, label='Mean Value')
+ax.plot(generations, best, label='Best Value')
+plt.xlabel('Generation')
+plt.ylabel('Value [$]')
+plt.grid()
+plt.show()
